882140afce53cb4052876bfc7a60822f23913a64
Josh484 <josh.m.mendoza1@gmail.com>
2024-03-07 20:15:17 +0000

    completed starting files, added tailwind, trying out tailwind

---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <div>
+                <h1 className="text-3xl font-bold text-purple-500">
+                    Js, Kitchen
+                </h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
